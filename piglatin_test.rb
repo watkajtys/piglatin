3cd4d0fcc2d0030af0189898b276d8941ca359bf
Matt Watkajtys <watkajtys@gmail.com>
2013-03-11 19:26:04 -0400

    Added Tests for Piglatin Converter

--- piglatin_test.rb
+++ piglatin_test.rb
@@ -2,5 +2,34 @@ require 'test/unit'
 require_relative 'piglatin_converter'
 
 class TestAdd < Test::Unit::TestCase
-  # Your tests go here
+  def test_string_with_vowel
+  	assert_equal "igpay", PiglatinConverter.convert("pig")
+  	assert_equal "omputercay", PiglatinConverter.convert("computer")
+  	assert_equal "eedomfray", PiglatinConverter.convert("freedom")
+  	assert_equal "ingstray", PiglatinConverter.convert("string")
+  end
+
+  def test_string_with_capital
+  	assert_equal "ingstray", PiglatinConverter.convert("String")
+  	assert_equal "ingstray", PiglatinConverter.convert("StrinG")
+  end
+
+  def test_string_empty
+  	assert_equal '<EMPTY STRING>', PiglatinConverter.convert('<EMPTY STRING>')
+  end
+
+  def test_nil_string
+  	assert_equal nil, PiglatinConverter.convert('')
+  end
 end
+
+
+
+#pig                igpay
+#   computer        omputercay
+#   freedom         eedomfray
+#   string          ingstray
+#   String          ingstray
+#   StrinG          ingstray
+#   <EMPTY STRING>  <EMPTY STRING>
+#   nil             nil
