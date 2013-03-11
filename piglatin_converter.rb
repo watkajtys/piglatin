30cdc5db4918c04f021dd1c6f3bafa1921ec8deb
Matt Watkajtys <watkajtys@gmail.com>
2013-03-11 19:54:35 -0400

    First attempt at getting converter working. Does indeed return piglatinified words, but needs hardcore refactoring

--- piglatin_converter.rb
+++ piglatin_converter.rb
@@ -1,7 +1,23 @@
 class PiglatinConverter
 
   def self.convert(word)
-    # Your code goes here
+   word = word.downcase
+   pig = 'ay'
+   p word
+   p consonant = word.split(/[aeiou]/)
+   p another = consonant.shift
+   p final = consonant << another
+   p final.join
+   p word
+   p "result "
+   p first = word.slice!(another)
+   p word+first+pig
+
   end
 
 end
+
+PiglatinConverter.convert('computer')
+PiglatinConverter.convert('freedom')
+PiglatinConverter.convert('string')
+PiglatinConverter.convert('strong')
