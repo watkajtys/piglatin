f56d4810ee00e2ff3112fe72a33d79ca4d3427bf
Matt Watkajtys <watkajtys@gmail.com>
2013-03-12 19:12:31 -0400

    Final refactor with regex. All tests work and it kicks ass.

--- piglatin_converter.rb
+++ piglatin_converter.rb
@@ -1,26 +1,9 @@
 class PiglatinConverter
-
 	def self.convert(word)
 		return word if word == nil || word == ''
-		puts word + " is the input word"
-		puts "This is the first consonant set before the vowel."
-		p consonant_set = /^(.*?)[aeiou]/.match(word.downcase).captures
-		puts "This is the fist consonant set as a string"
-		p consonant_string = consonant_set.join('')
-		puts "This is the final output"
-		p word.gsub(consonant_string, '') + consonant_string + 'ay'
-		
+		word_down = word.downcase
+		consonant_set = /^(.*?)[aeiou]/.match(word_down).captures
+		consonant_string = consonant_set.join('')
+		word_down.gsub(consonant_string, '') + consonant_string + 'ay'
 	end
- 
 end
-
-
-	# def self.convert(word)
-	#   	return word if word == nil || word == ''
-	# 	word = word.downcase
-	# 	consonant_set = word.split(/[aeiou]/)
-	# 	first_consonant = consonant_set.shift
-	# 	consonant_at_end = consonant_set << first_consonant
-	# 	first_set = word.slice!(first_consonant)
-	# 	word+first_set+'ay'
-	# end
