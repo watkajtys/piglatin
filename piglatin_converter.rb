f06f40025722b738f201a64b11cc206abfb16fbf
Matt Watkajtys <watkajtys@gmail.com>
2013-03-12 18:13:13 -0400

    Final commit before major refactor.

--- piglatin_converter.rb
+++ piglatin_converter.rb
@@ -2,15 +2,11 @@ class PiglatinConverter
 
   	def self.convert(word)
 	  	return word if word == nil || word == ''
-			word = word.downcase
-			consonant_set = word.split(/[aeiou]/)
-			first_consonant = consonant_set.shift
-			consonant_at_end = consonant_set << first_consonant
-			first_set = word.slice!(first_consonant)
-			word+first_set+'ay'
+		word = word.downcase
+		consonant_set = word.split(/[aeiou]/)
+		first_consonant = consonant_set.shift
+		consonant_at_end = consonant_set << first_consonant
+		first_set = word.slice!(first_consonant)
+		word+first_set+'ay'
 	end
 end
-
-# PiglatinConverter.convert('computer')
-# PiglatinConverter.convert('')
-# PiglatinConverter.convert(nil)
