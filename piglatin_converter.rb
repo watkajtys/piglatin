01a1989b342975c1dd36523cd0ac82348afe80cb
Matt Watkajtys <watkajtys@gmail.com>
2013-03-12 18:59:16 -0400

    First attempt at refactor with regex. String return is complete but first vowel is getting clipped

--- piglatin_converter.rb
+++ piglatin_converter.rb
@@ -1,12 +1,24 @@
 class PiglatinConverter
 
-  	def self.convert(word)
-	  	return word if word == nil || word == ''
-		word = word.downcase
-		consonant_set = word.split(/[aeiou]/)
-		first_consonant = consonant_set.shift
-		consonant_at_end = consonant_set << first_consonant
-		first_set = word.slice!(first_consonant)
-		word+first_set+'ay'
+	def convert(word)
+		return word if word == nil || word == ''
+		p consonant_set = /^(.*?)[aeiou]/.match(word.downcase).captures
+		p final = word.sub(/^(.*?)[aeiou]/, '')
+		p final + consonant_set.join('') + 'ay'
+		
 	end
+ 
 end
+
+converter = PiglatinConverter.new
+converter.convert('string')
+
+	# def self.convert(word)
+	#   	return word if word == nil || word == ''
+	# 	word = word.downcase
+	# 	consonant_set = word.split(/[aeiou]/)
+	# 	first_consonant = consonant_set.shift
+	# 	consonant_at_end = consonant_set << first_consonant
+	# 	first_set = word.slice!(first_consonant)
+	# 	word+first_set+'ay'
+	# end
